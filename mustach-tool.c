c72e4cf09dde3c1145988893d63194b3f95374c7
José Bollo <jobol@nonadev.net>
2019-03-12 17:01:32 +0100

    Fix pipe/stream of the tool
    The tool wasn't accepting to read streams and pipes.
    This changes allows it to process such devices.
    
    Signed-off-by: José Bollo <jobol@nonadev.net>

--- mustach-tool.c
+++ mustach-tool.c
@@ -31,6 +31,8 @@
 
 #include "mustach-json-c.h"
 
+static const size_t BLOCKSIZE = 8192;
+
 static const char *errors[] = {
 	"??? unreferenced ???",
 	"system",
@@ -51,16 +53,59 @@ static void help(char *prog)
 
 static char *readfile(const char *filename)
 {
-	struct stat s;
 	int f;
+	struct stat s;
 	char *result;
+	size_t size, pos;
+	ssize_t rc;
 
-	f = open(filename, O_RDONLY);
+	result = NULL;
+	if (filename[0] == '-' &&  filename[1] == 0)
+		f = dup(0);
+	else
+		f = open(filename, O_RDONLY);
+	if (f < 0) {
+		fprintf(stderr, "Can't open file: %s\n", filename);
+		exit(1);
+	}
+
 	fstat(f, &s);
-	result = malloc(s.st_size + 1);
-	read(f, result, s.st_size);
+	switch (s.st_mode & S_IFMT) {
+	case S_IFREG:
+		size = s.st_size;
+		break;
+	case S_IFSOCK:
+	case S_IFIFO:
+		size = BLOCKSIZE;
+		break;
+	default:
+		fprintf(stderr, "Bad file: %s\n", filename);
+		exit(1);
+	}
+
+	pos = 0;
+	result = malloc(size + 1);
+	do {
+		if (result == NULL) {
+			fprintf(stderr, "Out of memory\n");
+			exit(1);
+		}
+		rc = read(f, &result[pos], (size - pos) + 1);
+		if (rc < 0) {
+			fprintf(stderr, "Error while reading %s\n", filename);
+			exit(1);
+		}
+		if (rc > 0) {
+			pos += (size_t)rc;
+			if (pos > size) {
+				size = pos + BLOCKSIZE;
+				result = realloc(result, size + 1);
+			}
+		}
+	} while(rc > 0);
+
 	close(f);
-	result[s.st_size] = 0;
+	result[pos] = 0;
 	return result;
 }
 
