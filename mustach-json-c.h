ee3d9f7668d4a917aed5fa651e8dae8247627844
Jose Bollo <jose.bollo@iot.bzh>
2021-05-31 22:53:43 +0200

    Mark old function as deprecated
    It is valuable to report the deprecationi, conforming to the README.
    
    Fixes issue #29

--- mustach-json-c.h
+++ mustach-json-c.h
@@ -91,12 +91,70 @@ extern int mustach_json_c_write(const char *template, size_t length, struct json
  */
 extern int mustach_json_c_emit(const char *template, size_t length, struct json_object *root, int flags, mustach_emit_cb_t *emitcb, void *closure);
 
-/* compatibility with versions before 1.0.0 */
+/***************************************************************************
+* compatibility with version before 1.0
+*/
 
+/**
+ * OBSOLETE use mustach_json_c_file
+ *
+ * fmustach_json_c - Renders the mustache 'template' in 'file' for 'root'.
+ *
+ * @template: the template string to instanciate
+ * @root:     the root json object to render
+ * @file:     the file where to write the result
+ *
+ * Returns 0 in case of success, -1 with errno set in case of system error
+ * a other negative value in case of error.
+ */
+
+DEPRECATED_MUSTACH(extern int fmustach_json_c(const char *template, struct json_object *root, FILE *file));
+
+/**
+ * OBSOLETE use mustach_json_c_fd
+ *
+ * fdmustach_json_c - Renders the mustache 'template' in 'fd' for 'root'.
+ *
+ * @template: the template string to instanciate
+ * @root:     the root json object to render
+ * @fd:       the file descriptor number where to write the result
+ *
+ * Returns 0 in case of success, -1 with errno set in case of system error
+ * a other negative value in case of error.
+ */
+
+DEPRECATED_MUSTACH(extern int fdmustach_json_c(const char *template, struct json_object *root, int fd));
+
+/**
+ * OBSOLETE use mustach_json_c_mem
+ *
+ * mustach_json_c - Renders the mustache 'template' in 'result' for 'root'.
+ *
+ * @template: the template string to instanciate
+ * @root:     the root json object to render
+ * @result:   the pointer receiving the result when 0 is returned
+ * @size:     the size of the returned result
+ *
+ * Returns 0 in case of success, -1 with errno set in case of system error
+ * a other negative value in case of error.
+ */
+
+DEPRECATED_MUSTACH(extern int mustach_json_c(const char *template, struct json_object *root, char **result, size_t *size));
+
+/**
+ * OBSOLETE use mustach_json_c_write
+ *
+ * umustach_json_c - Renders the mustache 'template' for 'root' to custom writer 'writecb' with 'closure'.
+ *
+ * @template: the template string to instanciate
+ * @root:     the root json object to render
+ * @writecb:  the function that write values
+ * @closure:  the closure for the write function
+ *
+ * Returns 0 in case of success, -1 with errno set in case of system error
+ * a other negative value in case of error.
+ */
 typedef mustach_write_cb_t *mustach_json_write_cb;
-extern int fmustach_json_c(const char *template, struct json_object *root, FILE *file);
-extern int fdmustach_json_c(const char *template, struct json_object *root, int fd);
-extern int mustach_json_c(const char *template, struct json_object *root, char **result, size_t *size);
-extern int umustach_json_c(const char *template, struct json_object *root, mustach_write_cb_t *writecb, void *closure);
+DEPRECATED_MUSTACH(extern int umustach_json_c(const char *template, struct json_object *root, mustach_write_cb_t *writecb, void *closure));
 
 #endif
