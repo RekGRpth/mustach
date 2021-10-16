e5a945b960132e1713c4dcab3bd9641c1c6133d1
Jose Bollo <jose.bollo@iot.bzh>
2021-10-16 11:43:36 +0200

    Fix flags of test-custom-write

--- test6/test-custom-write.c
+++ test6/test-custom-write.c
@@ -134,7 +134,7 @@ int main(int ac, char **av)
 				mode = None;
 			else {
 				t = readfile(*av);
-				s = mustach_json_c_write(t, 0, o, -1, uwrite, NULL);
+				s = mustach_json_c_write(t, 0, o, Mustach_With_AllExtensions, uwrite, NULL);
 				if (s != 0)
 					fprintf(stderr, "Template error %d\n", s);
 				free(t);
